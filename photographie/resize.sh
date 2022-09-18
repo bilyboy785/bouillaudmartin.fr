7404560fc961ee772ccd369c7142dafd15afb63d
Martin Bouillaud <martin.bouillaud@fr.clara.net>
2022-09-18 10:17:57 +0100

    add photographie folder

---
+++ photographie/resize.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+THUMBS_DIR="./images/thumbs"
+FULL_DIR="./images/fulls"
+
+for IMAGE in $(ls ./images/optimizing)
+do
+    echo "Working thumbs for ${IMAGE}"
+    convert -resize 30% ./images/optimizing/${IMAGE} ${THUMBS_DIR}/${IMAGE}
+    cwebp -quiet -q 75 ./images/optimizing/${IMAGE} -o ${THUMBS_DIR}/${IMAGE}.webp
+    convert -quality 75 ./images/optimizing/${IMAGE} ${THUMBS_DIR}/${IMAGE}.avif
+    echo "Working fulls for ${IMAGE}"
+    convert -resize 60% ./images/optimizing/${IMAGE} ${FULL_DIR}/${IMAGE}
+    cwebp -quiet -q 80 ./images/optimizing/${IMAGE} -o ${FULL_DIR}/${IMAGE}.webp
+    convert -quality 80 ./images/optimizing/${IMAGE} ${FULL_DIR}/${IMAGE}.avif
+    echo "Cleaning optimizing directory"
+    rm -f ./images/optimizing/${IMAGE}
+done
